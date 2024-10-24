89bd643001b00547c11d6941ccdf0d9ae46f3a4a
Phani Charan <phanicharan.21@gmail.com>
2024-10-24 01:28:46 -0400

    initial commit

---
+++ src/test/java/dev/phani/roleanduribasedsecurity/RoleanduribasedsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phani.roleanduribasedsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleanduribasedsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
